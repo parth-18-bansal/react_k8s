63630c23e63af2995f0ffae0b8f80f60d1a835b3
parth-18-bansal <parthbansalcoding@gmail.com>
2025-04-02 23:31:27 +0530

    change the k8s files config

--- k8s/k8s_construction.sh
+++ k8s/k8s_construction.sh
@@ -2,3 +2,9 @@
 
 # create a cluster
 kind create cluster --name k8s-cluster --config ./k8s/cluster.yml
+
+# create a service
+kubectl apply -f ./k8s/frontend_service.yml
+
+# create a deployment
+kubectl apply -f ./k8s/frontend_deployment.yml
