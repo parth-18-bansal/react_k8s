38c1427d073e79456a046b33cad9cdbb84df6191
parth-18-bansal <parthbansalcoding@gmail.com>
2025-04-02 23:57:32 +0530

    testing

--- k8s/k8s_construction.sh
+++ k8s/k8s_construction.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # create a cluster
-kind create cluster --name k8s-cluster --config ./k8s/cluster.yml
+kind create cluster --name k8s-cluster --config cluster.yml
 
 # create a service
-kubectl apply -f ./k8s/frontend_service.yml
+kubectl apply -f frontend_service.yml
 
 # create a deployment
-kubectl apply -f ./k8s/frontend_deployment.yml
+kubectl apply -f frontend_deployment.yml
