2e4b53e462f1fb98df2530561626fe5729d08563
parth-18-bansal <parthbansalcoding@gmail.com>
2025-04-04 22:41:10 +0530

    deployed the flask backend in the k8s

--- k8s/k8s_construction.sh
+++ k8s/k8s_construction.sh
@@ -8,3 +8,6 @@ kubectl apply -f frontend_service.yml
 
 # create a deployment
 kubectl apply -f frontend_deployment.yml
+
+# create a backend flask deployment and service
+kubectl apply -f backend_flask_deployment_service.yml
