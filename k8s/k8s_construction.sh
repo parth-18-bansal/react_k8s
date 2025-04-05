d2f577c2e200d26b79b6a519f2058b12d0c9c14a
parth-18-bansal <parthbansalcoding@gmail.com>
2025-04-05 19:57:11 +0530

    deployed the database on the statefulset

--- k8s/k8s_construction.sh
+++ k8s/k8s_construction.sh
@@ -11,3 +11,9 @@ kubectl apply -f frontend_deployment.yml
 
 # create a backend flask deployment and service
 kubectl apply -f backend_flask_deployment_service.yml
+
+# create a pv
+kubectl apply -f persistence_volume.yml
+
+# create a mysql statefulset and other things
+kubectl apply -f mysql_statefulset_services.yml
