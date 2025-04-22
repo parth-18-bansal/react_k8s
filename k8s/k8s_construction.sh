22602684fe13830c5cca1e46789cddff44b1e980
parth-18-bansal <parthbansalcoding@gmail.com>
2025-04-22 16:01:33 +0530

    testing

--- k8s/k8s_construction.sh
+++ k8s/k8s_construction.sh
@@ -7,13 +7,13 @@ kind create cluster --name k8s-cluster --config cluster.yml
 kubectl apply -f frontend_service.yml
 
 # create a deployment
-kubectl apply -f frontend_deployment.yml
+#kubectl apply -f frontend_deployment.yml
 
 # create a backend flask deployment and service
-kubectl apply -f backend_flask_deployment_service.yml
+#kubectl apply -f backend_flask_deployment_service.yml
 
 # create a pv
-kubectl apply -f persistence_volume.yml
+#kubectl apply -f persistence_volume.yml
 
 # create a mysql statefulset and other things
-kubectl apply -f mysql_statefulset_services.yml
+#kubectl apply -f mysql_statefulset_services.yml
